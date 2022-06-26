5cbe6c2e8c37030c1acf9a16bf70b4997fae6040
MTmtEmpty <44959800+MatthewTao@users.noreply.github.com>
2022-06-26 22:09:32 +1000

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from src.utils import next_day_of_week
+from src.utils import next_day_of_week, determine_number_suffix
 
 from datetime import datetime
 import unittest
@@ -11,3 +11,19 @@ class TestUtils(unittest.TestCase):
         next_monday = next_day_of_week(0, now)
         print(next_monday)
         self.assertEqual(next_monday.weekday(), 0)
+
+    def test_determine_number_suffix(self):
+        numbers = [
+            {"value": 1, "expected": "st"},
+            {"value": 2, "expected": "nd"},
+            {"value": 3, "expected": "rd"},
+            {"value": 4, "expected": "th"},
+            {"value": 323, "expected": "rd"},
+            {"value": 101, "expected": "st"},
+            {"value": 452, "expected": "nd"}
+        ]
+        for number in numbers:
+            test_value = number.get('value')
+            expected = number.get('expected')
+            actual = determine_number_suffix(test_value)
+            self.assertEqual(actual, expected)
